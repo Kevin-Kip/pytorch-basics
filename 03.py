41cd482e59d8c3deb1600536f921330e52369446
Kevin-Kip <masterfork5@gmail.com>
2018-11-23 12:05:58 +0300

    Got the ssoftmax function working :smile:

--- 03.py
+++ 03.py
@@ -1,11 +1,24 @@
-import nupy as np
+import numpy as np
 
-def perceptron(x1,x2):
-    pass
+e = np.e
 
+# SOftmax using numpy
 def softmax(x):
     e_x = np.exp(x - np.max(x))
     return e_x / e_x.sum(axis=0)
 
+# A very inneficient version of softmax
+def plainmax(x):
+    probs = 0 # this holds the (e^z1+e^z2+e^z3)
+    results = []
+    for item in x:
+        probs += (e ** item)
+
+    for each in x:
+        results.append((e ** each) / probs)
+
+    return results
+
 if __name__ == '__main__':
-    pass
+    print(softmax([2,2]))
+    print(plainmax([2,2]))
