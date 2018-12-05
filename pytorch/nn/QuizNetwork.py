ac486b37f2196be436b435ccda57ecf7ed93e8a9
Kevin-Kip <masterfork5@gmail.com>
2018-12-05 09:56:01 +0300

    Completed quiz to create a neural ne with 2 hidden layers

---
+++ pytorch/nn/QuizNetwork.py
@@ -0,0 +1,20 @@
+from torch import nn
+from torch import torch.nn.functional as F
+
+class QuizNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.hidden1 = nn.Linear(784, 128)
+        self.hidden2 = nn.Linear(128, 64)
+        self.output = nn.Linear(64, 10)
+
+    def forward(self, x):
+        x = self.hidden1(x)
+        x = F.relu(x)
+        x = self.hidden2(x)
+        x = F.relu(x)
+        x = self.output(x)
+        x = self.softmax(x, dim=1)
+
+        return x
