f28d94eb358158a56226031ae797de017a913ebc
Kevin-Kip <masterfork5@gmail.com>
2018-12-05 09:58:34 +0300

    Added print lines and fixed import issue

--- pytorch/nn/QuizNetwork.py
+++ pytorch/nn/QuizNetwork.py
@@ -1,5 +1,5 @@
 from torch import nn
-from torch import torch.nn.functional as F
+import torch.nn.functional as F
 
 class QuizNetwork(nn.Module):
     def __init__(self):
@@ -18,3 +18,8 @@ class QuizNetwork(nn.Module):
         x = self.softmax(x, dim=1)
 
         return x
+
+m = QuizNetwork()
+# shows the weights and bias of hidden layer 1
+print(m.hidden1.bias)
+print(m.hidden1.weight)
